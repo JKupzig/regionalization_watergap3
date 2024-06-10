d2da44e6498e455cd46bc831060c5a19b390cf37
JKupzig <jenny.kupzig@posteo.de>
2024-06-10 14:52:38 +0200

    modifiy regionalization approache for WG3 and MLR and B2B

--- main_get_exponents.r
+++ main_get_exponents.r
@@ -11,27 +11,12 @@ library(dplyr)
 source("./algorithm/apply.MultipleLinearRegression.R")
 source("./helper_functions.r")
 
-# ============================================================================
-# Prepare Logging
-# ============================================================================
-my_logfile <- "results_exponents.txt"
-if (file.exists(my_logfile)) { file.remove(my_logfile) }
-my_console_appender = log4r::console_appender(layout = log4r::default_log_layout())
-my_file_appender <- log4r::file_appender(my_logfile,
-                                 append = TRUE,
-                                 layout = log4r::default_log_layout())
-my_logger <- log4r::logger(threshold = "INFO",
-                           appenders = list(my_console_appender,my_file_appender))
 
 # ============================================================================
 # Loading Variables
 # ============================================================================
 MIN_SIZE <- 5000
 MIN_QUALITY <- 0.2
-SAMPLING_NUMBER <- 100
-
-log4r::info(my_logger, sprintf("settings: min_size=%g, min_quality=%f, n_samples=%g",
-                               MIN_SIZE, MIN_QUALITY, SAMPLING_NUMBER))
 
 #Load Vars
 ROOT <- "./data"
@@ -62,96 +47,31 @@ names(x_orig[columns_b2b])
 ################################################################################
 # getting coefficients
 ################################################################################
-coefficients_new <- NULL
-maes <- NULL
-pb <- txtProgressBar(min = 1, max = SAMPLING_NUMBER, style = 3)
-set.seed(123)
 
-regression_to_save_mlr <- list()
-for (i in 1:SAMPLING_NUMBER) {
-  setTxtProgressBar(pb, i)
-  ind <- sample(2, nrow(red_x_orig), replace = TRUE, prob = c(0.50, 0.50))
+x=red_x_orig[,columns_b2b]
+y=red_y$mean_gamma
+data=x
+data$gamma=y
+b2b <- lm(log(data$gamma) ~ ., data)
+gamma_b2b <- exp(predict(b2b, newdata=data))
+gamma_b2b <- limit_gamma_to_be_valid(gamma_b2b,
+                                     upper_limit=5.,
+                                     lower_limit=0.1)
 
-
-  regression <- run.MultipleLinearRegression(
-    ind,
-    red_x_orig[columns_new_method],
-    red_y$mean_gamma,
-    tuningPars = c(lower_bound_tuning, upper_bound_tuning),
-    ln="off",
-    return_regression = TRUE
-  )
-
-  regression_to_save_mlr[[i]] <- regression
-
-  y_est <- predict(regression, red_x_orig[columns_new_method])
-  y_est_tuned <- limit_gamma_to_be_valid(y_est, upper=upper_bound_tuning,
-                          lower = lower_bound_tuning)
-  bm <- mean(abs(y_est_tuned - red_y$mean_gamma))
-  coefficients_new <- rbind(coefficients_new, regression$coefficients)
-  maes <- c(maes, bm)
-}
-
-idx <- which(maes == min(maes))
-saveRDS(regression_to_save_mlr[[idx]], "./data/regression_mlr.rds")
-
-coeffs_new <- coefficients_new[idx, ]
-log4r::info(my_logger, "#####################################")
-log4r::info(my_logger, "Info to use for MLR approach:")
-log4r::info(my_logger, sprintf("Tuning pars: %f & %f", lower_bound_tuning, upper_bound_tuning))
-for (name in names(coeffs_new)) {
-  log4r::info(my_logger, sprintf("%s: %f", name, coeffs_new[name]))
-}
+x=red_x_orig[,columns_new_method]
+y=red_y$mean_gamma
+data=x
+data$gamma=y
+mlr <- lm(data$gamma ~ ., data)
+gamma_mlr <- exp(predict(mlr, newdata=data))
+gamma_mlr <- limit_gamma_to_be_valid(gamma_mlr,
+                                     upper_limit=upper_bound_tuning,
+                                     lower_limit=lower_bound_tuning)
 
 
-set.seed(123)
-coefficients_b2b <- NULL; b2b_maes <- NULL
-regression_to_save_b2b <- list()
-for (i in 1:SAMPLING_NUMBER){
-  setTxtProgressBar(pb, i)
-  ind <- sample(2, nrow(red_x_orig), replace = TRUE, prob = c(0.50, 0.50))
+mlr_quality <- mae(obs=log(y+1), sim=log(gamma_mlr+1))
+b2b_quality <- mae(obs=log(y+1), sim=log(gamma_b2b+1))
 
-  regression <- run.MultipleLinearRegression(
-    ind,
-    red_x_orig[columns_b2b],
-    red_y$mean_gamma,
-    tuningPars = c(lower_bound_tuning, upper_bound_tuning),
-    ln="on",
-    return_regression = TRUE
-  )
-
-  regression_to_save_b2b[[i]] <- regression
-  y_est <- predict(regression, red_x_orig[columns_b2b])
-
-  y_est_limited <- limit_gamma_to_be_valid(exp(y_est),
-                                           upper=5,
-                                           lower = 0.1)
-
-  bm <- mean(abs(y_est_limited - red_y$mean_gamma))
-  b2b_maes <- c(b2b_maes, bm)
-  coefficients_b2b <- rbind(coefficients_b2b, regression$coefficients)
-}
-
-idx <- which(b2b_maes == min(b2b_maes))
-coeffs_wg2 <- coefficients_b2b[idx,]
-saveRDS(regression_to_save_b2b[[idx]], "./data/regression_b2b.rds")
-
-log4r::info(my_logger, "#####################################")
-log4r::info(my_logger, "Info to use for WG2 approach:")
-for (name in names(coeffs_wg2)) {
-  log4r::info(my_logger, sprintf("%s: %f", name, coeffs_wg2[name]))
-}
-
-
-######################################
-
-# evaluating model for all gauged basins
-y_est_new <- new_model(
-  red_x_orig[columns_new_method],
-  coeffs_new,
-  lower_bound_tuning, upper_bound_tuning)
-
-y_est_wg2 <- wg2_model(
-  red_x_orig[columns_b2b],-
-  coeffs_wg2)
+saveRDS(b2b, "./data/regression_b2b.rds")
+saveRDS(mlr, "./data/regression_mlr.rds")
 
