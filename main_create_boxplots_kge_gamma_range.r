78420ddab27457a4c917202c45e3cb07d39e3c4b
JKupzig <jenny.kupzig@posteo.de>
2024-06-16 12:31:19 +0200

    add data and scripts for fig 4

--- main_create_boxplots_kge_gamma_range.r
+++ main_create_boxplots_kge_gamma_range.r
@@ -18,68 +18,23 @@ MIN_QUALITY <- 0.2
 
 x_orig <- readRDS(file.path(ROOT, "NEW_x_orig.rds"))
 y <- readRDS(file.path(ROOT, "NEW_y.rds"))
-distance_to_centroid <- readRDS(file.path(ROOT, "NEW_distCentroid.rds"))
+kge_all <- read.table(file.path(ROOT, "KGE.txt"), header=T)
 
 reducer <- create_subset(MIN_QUALITY, MIN_SIZE, use_kge ="on")
-red_x_orig <- x_orig[reducer, ]
 red_y <- y[reducer, ]
-red_distance_to_centroid <- distance_to_centroid[reducer, reducer]
 
-set.seed(123)
-ind_list <- list()
-for (i in 1:100){
-  ind <- sample(2, nrow(red_x_orig), replace = TRUE, prob = c(0.50, 0.50))
-  ind_list[[i]] <- ind
-}
-ind <- ind_list[[48]]
-
-template <- NULL
-
-list_to_iterate <- list(
-  "CAL"= list(2, "CAL", template),
-  "MLRGOOD"=list(2, "MLRGOOD48", template),
-  "MLRBAD"=list(2, "MLRBAD48", template),
-  "KNNGOOD"=list(2, "KNNGOOD48",template),
-  "KNNBAD"=list(2, "KNNBAD48",template),
-  "SIGOOD"=list(2, "SIGOOD48",template),
-  "SIBAD"=list(2, "SIBAD48",template),
-  #"KMEANSGOOD"=list(2, "KMEANSGOOD48",template),
-  #"KMEANSBAD"=list(2, "KMEANSBAD48",template),
-  "SP"=list(2, "SP48",template),
-  "B2B"= list(2, "B2B48", template),
-  "DONOR"=list(1, "CAL",template)
-)
-
-for (name in names(list_to_iterate)){
-
-  for (cont in c("af","au", "as", "eu", "na", "sa"))
-  {
-
-    name_to_read <- list_to_iterate[[name]][[2]]
-    ind_to_use <- list_to_iterate[[name]][[1]]
-    result_save <- list_to_iterate[[name]][[3]]
-
-    reduced_kge_cont <- read_kge(cont=cont, type=name_to_read,
-                                 red_y, red_x_orig, ind, ind_to_use)
-    reduced_kge_cont$runtype <- name
-
-    result_save <- rbind(result_save, reduced_kge_cont)
-    list_to_iterate[[name]][[3]] <- result_save
-  }
-}
-
-kge_all <- do.call(rbind, lapply(list_to_iterate, `[[`, 3))
 
 give.n <- function(x){
   return(c(y = 1.0, label = length(x)))
 }
 
 gamma_expression <- expression(paste("Calibrated ", gamma, " (-)"))
+list_to_iterate <- unique(kge_all$runtype)
 
 kge_all %>%
   merge(., red_y, by.x="station_id", by.y="ID", all.x=T) %>%
   filter(runtype != "DONOR") %>%
-  mutate(runtype=factor(runtype, levels=names(list_to_iterate))) %>%
+  mutate(runtype=factor(runtype, levels=list_to_iterate)) %>%
   mutate(gamma_groups = case_when(
   mean_gamma <= .2 ~ "0.1-0.2",
   mean_gamma > 0.2 & mean_gamma <= 0.5 ~ "0.2-.5",
--- main_kge_table_and_outliers.r
+++ main_kge_table_and_outliers.r
@@ -9,78 +9,7 @@ source("./helper_functions.r")
 library(ggplot2)
 library(dplyr)
 
-#Load Vars
-ROOT <- "./data"
-MIN_SIZE <- 5000
-MIN_QUALITY <- 0.2
-
-x_orig <- readRDS(file.path(ROOT, "NEW_x_orig.rds"))
-y <- readRDS(file.path(ROOT, "NEW_y.rds"))
-
-reducer <- create_subset(MIN_QUALITY, MIN_SIZE, use_kge ="on")
-red_x_orig <- x_orig[reducer, ]
-red_y <- y[reducer, ]
-
-set.seed(123)
-
-ind_list <- list()
-for (i in 1:100){
-  ind <- sample(2, nrow(red_x_orig), replace = TRUE, prob = c(0.50, 0.50))
-  ind_list[[i]] <- ind
-}
-ind <- ind_list[[48]]
-
-
-
-template <- NULL
-
-list_to_iterate <- list(
-  "CAL"= list(2, "CAL", template),
-  "MLRGOOD"=list(2, "MLRGOOD48", template),
-  "MLRBAD"=list(2, "MLRBAD48", template),
-  "KNNGOOD"=list(2, "KNNGOOD48",template),
-  "KNNBAD"=list(2, "KNNBAD48",template),
-  "SIGOOD"=list(2, "SIGOOD48",template),
-  "SIBAD"=list(2, "SIBAD48",template),
-  "SP"=list(2, "SP48",template),
-  "B2B"= list(2, "B2B48", template),
-  #"KMEANSGOOD"=list(2, "KMEANSGOOD48",template),
-  #"KMEANSBAD"=list(2, "KMEANSBAD48",template),
-  "DONOR"=list(1, "CAL",template)
-  )
-
-
-for (name in names(list_to_iterate)){
-
-  for (cont in c("af","au", "as", "eu", "na", "sa"))
-  {
-
-  name_to_read <- list_to_iterate[[name]][[2]]
-  ind_to_use <- list_to_iterate[[name]][[1]]
-  result_save <- list_to_iterate[[name]][[3]]
-
-  reduced_kge_cont <- read_kge(cont=cont, type=name_to_read,
-                               red_y, red_x_orig, ind, ind_to_use)
-  reduced_kge_cont$runtype <- name
-
-  result_save <- rbind(result_save, reduced_kge_cont)
-  list_to_iterate[[name]][[3]] <- result_save
-  }
-}
-
-remove_null_from_list <- function(list_to_analyse){
-  count <- 1
-  list_without_null <- list()
-  for (i in 1:length(list_to_analyse)){
-    if (!is.null(list_to_analyse[[i]])){
-      list_without_null[[count]] <- list_to_analyse[[i]]
-      count <- count + 1
-    }
-  }
-  return(list_without_null)
-}
-
-kge_all <- do.call(rbind, lapply(list_to_iterate, `[[`, 3))
+kge_all <- read.table("./data/KGE.txt", header=T)
 kge_all_summarized <- kge_all
 kge_all_summarized$cont = "global"
 kge_for_plot <- rbind(kge_all, kge_all_summarized)
@@ -92,13 +21,30 @@ overlapping_map <- kge_all %>%
   count(station_id) %>%
   as.data.frame()
 
+hist(overlapping_map$n, xlab="#Methods", ylab="#Basin with KGE \u2264 0.2", main="")
+dev.copy(png, file.path("./plots", "Figure_4c.png"),
+         width=12, height=12, units="cm", res=300)
+dev.off()
+
 # El Patatnito (gamma cal = 1.5)
-basins_all_not_estimatable <- which(overlapping_map$n == length(names(list_to_iterate))-2)
+MINUS_CAL_RESULT <- 2
+basins_all_not_estimatable <- which(overlapping_map$n == length(unique(kge_for_plot$runtype))-MINUS_CAL_RESULT)
 basin_id <- overlapping_map$station_id[basins_all_not_estimatable]
 estimates <- readRDS("./data/estimates.rds")
 estimates_difficult_basins <- estimates[estimates$basin_id %in% basin_id,]
-kge_all[kge_all$station_id %in% basin_id, ]
+results_outlier <- kge_all[kge_all$station_id %in% basin_id, ]
+print("outlier for all methods:")
+print(results_outlier)
 
+
+
+kge_analyse <- kge_all %>%
+  group_by(runtype) %>%
+  summarize(max=quantile(KGE, probs=1.0),
+            min=quantile(KGE, probs=0),
+            median=quantile(KGE, probs=0.5),
+            mean=mean(KGE)) %>%
+  as.data.frame()
 
 outliers <- kge_all %>%
   filter(.,KGE <= 0.2) %>%
@@ -106,36 +52,18 @@ outliers <- kge_all %>%
   count(runtype) %>%
   as.data.frame()
 
-hist(overlapping_map$n, xlab="#Methods", ylab="#Basin with KGE \u2264 0.2", main="")
-dev.copy(png, file.path("./plots", "Figure_4c.png"),
-         width=12, height=12, units="cm", res=300)
-dev.off()
+kge_analyse <- merge(outliers, kge_analyse, by="runtype")
+print("data for Table in Figure 4b")
+print(kge_analyse)
 
-
-# data for Table in Figure 4b
-kge_analyse <- kge_all %>%
-  filter(., !runtype %in% c("KMEANSGOOD", "KMEANSBAD")) %>%
-  group_by(runtype) %>%
-  summarize(max=quantile(KGE, probs=1.0),
-            min=quantile(KGE, probs=0),
-            median=quantile(KGE, probs=0.5),
-            mean=mean(KGE)) %>%
-  as.data.frame()
-kge_analyse[,c(2,4,3, 5)] <- round(kge_analyse[,c(2,4,3,5)], 3)
-
-merge(outliers, kge_analyse, by="runtype")
-
-# pairwise.wilcox.test --> argument to use allregionalization methods!
+# pairwise.wilcox.test
 kge_all_cal <- kge_all[!kge_all$runtype %in% c("DONOR", "CAL"),]
 kge_all_cal$runtype <- as.character(kge_all_cal$runtype)
-pairwise.wilcox.test(kge_all_cal$KGE,
-                     kge_all_cal$runtype, paired=T, exact = FALSE,
-                     p.adjust="bonferroni")
-
-pairwise.wilcox.test(kge_all_cal$KGE,
+kge_all_cal$runtype[kge_all_cal$runtype == "SP"] <- "S_P"
+wilcox <- pairwise.wilcox.test(kge_all_cal$KGE,
                      kge_all_cal$runtype, paired=T, exact = FALSE,
                      p.adjust="BH")
-
-pairwise.wilcox.test(kge_all_cal$KGE,
-                     kge_all_cal$runtype, paired=T, exact = FALSE,
-                     p.adjust="BY")
+result <- wilcox$p.value[which(rownames(wilcox$p.value) == "SIGOOD"),]
+also_good <- colnames(wilcox$p.value)[which(result >= 0.05)] #null hypothesis cannot be rejected --> no signficance difference in central tendecy
+print("equally well-performing methods:")
+print(also_good)
