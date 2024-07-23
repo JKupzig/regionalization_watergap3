874099dd424ba67980bda1ecc753b3f179b8d74c
JKupzig <jenny.kupzig@posteo.de>
2024-07-23 17:14:43 +0200

    add analysis for modified nse

--- main_kge_table_and_outliers.r
+++ main_kge_table_and_outliers.r
@@ -3,15 +3,16 @@
 
 # evaluates kge from split sample test in the context of low quality basins and creates Figure 4b and 4c
 
-rm(list=ls())
+rm(list = ls())
 
 source("./helper_functions.r")
 library(ggplot2)
 library(dplyr)
 
-kge_all <- read.table("./data/KGE.txt", header=T)
+kge_all <- read.table("./data/benchmarks_representative_sample.txt",
+                      header=TRUE)
 kge_all_summarized <- kge_all
-kge_all_summarized$cont = "global"
+kge_all_summarized$cont <- "global"
 kge_for_plot <- rbind(kge_all, kge_all_summarized)
 kge_for_plot$cont <- factor(kge_for_plot$cont,
                             levels=c("global", "af", "as", "au", "eu", "na", "sa"))
@@ -40,30 +41,53 @@ print(results_outlier)
 
 kge_analyse <- kge_all %>%
   group_by(runtype) %>%
-  summarize(max=quantile(KGE, probs=1.0),
-            min=quantile(KGE, probs=0),
-            median=quantile(KGE, probs=0.5),
-            mean=mean(KGE)) %>%
+  summarize(max_KGE = quantile(KGE, probs = 1.0),
+            min_KGE = quantile(KGE, probs = 0),
+            median_KGE = quantile(KGE, probs = 0.5),
+            mean_KGE = mean(KGE),
+            max_NSE1 = quantile(NSE_1, probs = 1.0),
+            min_NSE1 = quantile(NSE_1, probs = 0),
+            median_NSE1 = quantile(NSE_1, probs = 0.5),
+            mean_NSE1 = mean(NSE_1)) %>%
   as.data.frame()
+kge_analyse[,-c(1, 2)] <- round(kge_analyse[,-c(1, 2)], 3)
 
-outliers <- kge_all %>%
-  filter(.,KGE <= 0.2) %>%
+outliers_kge <- kge_all %>%
+  filter(., KGE <= 0.2) %>%
   group_by(runtype) %>%
   count(runtype) %>%
   as.data.frame()
 
-kge_analyse <- merge(outliers, kge_analyse, by="runtype")
+kge_analyse <- merge(outliers_kge, kge_analyse, by = "runtype")
 print("data for Table in Figure 4b")
 print(kge_analyse)
 
 # pairwise.wilcox.test
-kge_all_cal <- kge_all[!kge_all$runtype %in% c("DONOR", "CAL"),]
-kge_all_cal$runtype <- as.character(kge_all_cal$runtype)
-kge_all_cal$runtype[kge_all_cal$runtype == "SP"] <- "S_P"
-wilcox <- pairwise.wilcox.test(kge_all_cal$KGE,
-                     kge_all_cal$runtype, paired=T, exact = FALSE,
-                     p.adjust="BH")
-result <- wilcox$p.value[which(rownames(wilcox$p.value) == "SIGOOD"),]
-also_good <- colnames(wilcox$p.value)[which(result >= 0.05)] #null hypothesis cannot be rejected --> no signficance difference in central tendecy
-print("equally well-performing methods:")
-print(also_good)
+results_for_table <- NULL
+row_names <- NULL
+for (column in c("SIGOOD", "MLRGOOD")) {
+  new_column <- sprintf("X_%s", column)
+  for (benchmark in c("NSE_1", "KGE")) {
+    kge_all_cal <- kge_all[!kge_all$runtype %in% c("DONOR", "CAL"),]
+    kge_all_cal$runtype <- as.character(kge_all_cal$runtype)
+    kge_all_cal$runtype[kge_all_cal$runtype == column] <- new_column
+    wilcox <- pairwise.wilcox.test(
+      kge_all_cal[[benchmark]],
+      kge_all_cal$runtype,
+      paired=TRUE, exact = FALSE,
+      alternative = "greater",
+      p.adjust="BH")
+    result <- wilcox$p.value[which(rownames(wilcox$p.value) == new_column),]
+    complete_result <- c(result, column=1)
+    names(complete_result) <- c(names(result), column)
+    correct_order <- result[order(names(complete_result))]
+
+    results_for_table <- rbind(results_for_table, correct_order)
+    row_names <- c(row_names, sprintf("%s_%s", column, benchmark))
+  }
+}
+
+data <- t(round(results_for_table, 3))
+colnames(data) <- row_names
+print(data)
+
